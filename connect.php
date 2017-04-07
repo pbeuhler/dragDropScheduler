a1d90851cfa3a84fa1098c13048d5b6816918eed
j4freeman <j4freeman@gmail.com>
2017-04-06 23:17:57 -0500

    added function to query database for values
    pass in an id of form ex: AE245, pass in parameter of type ID, Name,
    Prerequisites, Corequisites, Description, Credit, Semesters.

--- connect.php
+++
@@ -1,3 +0,0 @@
-<?php
-$mysqli  = new mysqli("mysql.eecs.ku.edu", "jfreeman67", "frozone", "jfreeman67");
-?>
--- dragDropScheduler.js
+++ dragDropScheduler.js
@@ -1,31 +1,48 @@
+var lotInfoDB;
+var oReq = new XMLHttpRequest();
+oReq.open("GET", "./448.db", true);
+oReq.responseType = "arraybuffer";
+oReq.onload = function(e) {
+  var uInt8Array = new Uint8Array(this.response);
+    classDB = new SQL.Database(uInt8Array);
+};
+oReq.send();
+
 function dragStart(ev) {
    ev.dataTransfer.effectAllowed='move';
    ev.dataTransfer.setData("Text", ev.target.getAttribute('id'));
    ev.dataTransfer.setDragImage(ev.target,0,0);
-
    return true;
 }
 
 function allowDrop(ev) {
-ev.preventDefault();
-info();
+  ev.preventDefault();
+  //info();
 }
-function drag(ev) {
-ev.dataTransfer.setData("text", ev.target.id);
+  function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
 }
 function drop(ev) {
-ev.preventDefault();
-var data = ev.dataTransfer.getData("text");
-ev.target.appendChild(document.getElementById(data));
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+  var test = getValue('Name',"AE430")
+  alert(test);
+}
+
+function getValue(colVal, id) {
+  var val = classDB.exec("SELECT " + colVal + " FROM project WHERE ID='" + id +"'");
+  console.log(val)
+  return (val[0].values[0][0]);
 }
 
 function h(){
-var start = document.getElementById("first").value;
-var end = document.getElementById("last").value;
+  var start = document.getElementById("first").value;
+  var end = document.getElementById("last").value;
 
-if(start > end || start < 2014){
-   alert("Incorrect inputs in Semester Generator section");
- }
+  if(start > end || start < 2014){
+    alert("Incorrect inputs in Semester Generator section");
+  }
  //else{
    //var x = (end - start)*2;
    //while(x!=0){
