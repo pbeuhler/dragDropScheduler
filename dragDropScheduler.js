b7d9fb1215222815b3ce8afa7a67dacd0443ff9c
j4freeman <j4freeman@gmail.com>
2017-04-18 10:32:24 -0500

    Fixed functionality to highlight courses that don't have their met prereqs
    Should all be working now

--- dragDropScheduler.js
+++ dragDropScheduler.js
@@ -1,6 +1,6 @@
 var lotInfoDB;
 var oReq = new XMLHttpRequest();
-oReq.open("GET", "./448.db", true);
+oReq.open("GET", "https://people.eecs.ku.edu/~jfreeman67/448.db", true);
 oReq.responseType = "arraybuffer";
 oReq.onload = function(e) {
   var uInt8Array = new Uint8Array(this.response);
@@ -8,8 +8,14 @@ oReq.onload = function(e) {
 };
 oReq.send();
 
-var semestersArray = ['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8'];
+var semestersArray = ['Prereqs', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8'];
+var dropArr = ['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8'];
 var notInDatabase = ['KUCore1', 'KUCore2', 'KUCore3', 'KUCore4', 'Tech1', 'KUCore5', 'Tech2', 'Tech3', 'KUCore6'];
+var Used = [];
+var preReq = [];
+var num_drops;
+
+
 
 function getChildren(divID) {
   var div = document.getElementById(divID);
@@ -27,22 +33,282 @@ function dragStart(ev) {
    ev.dataTransfer.effectAllowed='move';
    ev.dataTransfer.setData("Text", ev.target.getAttribute('id'));
    ev.dataTransfer.setDragImage(ev.target,0,0);
-   return true;
+
 }
 
 function allowDrop(ev) {
   ev.preventDefault();
   //info();
 }
-  function drag(ev) {
+
+function hover(ev) {
+  var x = document.querySelectorAll("*[draggable=true]");
+  for (i in x) {
+    x[i].style = "background:white";
+  }
+  divId = ev.getAttribute('id');
+  for (i in notInDatabase) {
+    if (divId == notInDatabase[i]) {
+      document.getElementById(divId).style = "background:PaleGreen";
+      return true;
+    }
+  }
+  Info.innerHTML = getValue('Name',divId) + "<br />" + getValue('Description',divId);
+  if (checkPrerequisitesMet(divId) == true) {
+    document.getElementById(divId).style = "background:PaleGreen";
+  }
+  else {
+    document.getElementById(divId).style = "background:IndianRed";
+    notMetPreArray = checkPrerequisitesMet(divId);
+    notMetCoArray = checkCorequisitesMet(divId);
+    if (notMetPreArray[0] == "senior") {
+      document.getElementById(divId).style = "background:PaleGreen";
+      return true;
+    }
+    for (i in notMetCoArray) {
+      document.getElementById(notMetCoArray[i]).style = "background:Wheat";
+    }
+    for (i in notMetPreArray) {
+      document.getElementById(notMetPreArray[i]).style = "background:Pink";
+    }
+  }
+}
+
+function leave(ev) {
+  // divId = ev.getAttribute('id');
+  // for (i in notInDatabase) {
+  //   if (divId == notInDatabase[i]) {
+  //     document.getElementById(divId).style = "background:white";
+  //     return true;
+  //   }
+  // }
+  // document.getElementById(divId).style = "background:white";
+  // notMetPreArray = checkPrerequisitesMet(divId);
+  // notMetCoArray = checkCorequisitesMet(divId);
+  // if (notMetCoArray != true) {
+  //   for (i in notMetCoArray) {
+  //     document.getElementById(notMetCoArray[i]).style = "background:white";
+  //   }
+  // }
+  // if (notMetCoArray != true) {
+  //   for (i in notMetPreArray) {
+  //     document.getElementById(notMetPreArray[i]).style = "background:white";
+  //   }
+  // }
+}
+
+function drag(ev) {
+	dragStart(ev);
   ev.dataTransfer.setData("text", ev.target.id);
 }
+
+function Position(data,target,rank){
+	this.course_id = data;
+	this.target = target;
+	this.rank = rank;
+
+	//refresh course position
+	this.refresh = function(num_drops){
+		for(i in Used){
+				var a = this.course_id;
+				var b = Used[i].course_id;
+				//same id tag
+				if(a == b && i<Used.length-1){
+					this.course_id = Used[i].course_id;
+					this.rank = num_drops;
+					var ind = Used.indexOf(Used[i]);
+					Used.splice(num_drops-1,1);
+				}
+			}
+	}
+}
+
+/* for refresh_array(source,prereq_name){
+	for(i in Used){
+				var a = source.course_id;
+				//same id tag
+				if(a == prereq_name && preReq[i].course_id == prereq_name){
+					source.course_id = Used[i].course_id;
+					source.target = ;
+					var ind = Used.indexOf(Used[i]);
+					preReq.splice(num_drops-1,1);
+				}
+			}
+} */
+
 function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
-  var prereqsMet = checkPrerequisitesMet(document.getElementById(data).id);
-  var semesterRight = checkSemesters(document.getElementById(data).id);
+
+
+  //if target is valid, it is in the semester array, then diff will be < 8
+  var diff = 0;
+  for(i in dropArr){
+	  if(ev.target.id != dropArr[i]){
+		  diff++;
+	  }
+  }
+  if(diff < 8){
+
+
+		//succesful drop
+		num_drops = num_drops+1;
+	  ev.target.appendChild(document.getElementById(data));
+		var prereqsMet = checkPrerequisitesMet(document.getElementById(data).id);
+		var coreqsMet = checkCorequisitesMet(document.getElementById(data).id);
+		var semesterRight = checkSemesters(document.getElementById(data).id);
+		var credits = getCreditsSemester(ev.target.id);
+    // if (prereqsMet != true) {
+    //   for (i in range prereqsMet) {
+    //     document.getElementById(prereqsMet[i]).style = "background:white";
+    //   }
+    // }
+    // if (coreqsMet != true) {
+    //   for (i in range coreqsMet) {
+    //     document.getElementById(coreqsMet[i]).style = "background:white";
+    //   }
+    // }
+		//Info.innerHTML = getValue('Description',document.getElementById(data).id);
+
+		//console.log(document.getElementById(data).id);
+		//console.log(prereqsMet);
+		//console.log(coreqsMet);
+		//console.log(semesterRight);
+		//console.log(credits);
+		var id = document.getElementById(data).id;
+
+
+
+				//generates position
+				var y = new Position(id, ev.target.id,num_drops);
+				Used.push(y);
+				console.log(y.course_id);
+				num_drops = num_drops+1;
+				//refresh all coruses location // substitute old location
+				y.refresh(num_drops);
+				//object with prerequisite and its prerequisites
+				// pre(y,prereqsMet);
+				//console.log(prereqsMet);
+
+  }
+  console.log("-------");
+}//end drop
+
+//color
+function pre(obj, array){
+	var pre;
+	var sor;
+  if(preReq[0] != ""){
+	  for(i in array){
+		for(j in Used){
+				//obtain prerequisite through Used array
+				//verify prerequisite instance is in invalid position
+
+				if((Used[j].course_id != array[i]) && Used[j].target >= obj.target){
+					sor = Used[j].course_id;
+					console.log("white2");
+					//source
+					document.getElementById(obj.course_id).style = "background:red";
+					////prerequisite
+					document.getElementById(array[i]).style = "background:pink";
+					preReq.push([obj, array[i]]);
+				}
+
+				else if((Used[j].course_id == array[i]) && Used[j].target >= obj.target){
+					pre = Used[j].course_id;
+					console.log("white2");
+					//source
+					document.getElementById(obj.course_id).style = "background:red";
+					////prerequisite
+					document.getElementById(array[i]).style = "background:pink";
+					preReq.push([obj, array[i]]);
+				}
+
+
+			//	}
+			}
+		}
+
+
+	  //refresh prereq array
+	  var x =0;
+	  var h = 0;
+		while(obj.course_id != array[x] && x<preReq.length){
+			console.log(preReq.length)
+			var a = obj.target;
+				//same id tag
+				if(preReq[x][0].course_id == obj.course_id){
+					if(h==0){
+					preReq[x][0].target = obj.target;
+					h++;
+					}
+					else{
+						preReq.splice(x,1);
+					}
+				}
+			x++;
+		}
+		//console.log(preReq.length);
+	  //look how many conflicts current drop solved
+	  for(i in preReq){
+		 //console.log(i);
+		//ideal courses' conditions
+		//source -> preReq[i][0]
+		//prerequisite -> preReq[i][1]
+		//if either source or prerequisite is being moved check relative position
+
+		//if prerequiste is object beg moved
+		console.log(preReq[i][0],preReq[i][1])
+		if((preReq[i][1] == obj.course_id && preReq[i][0].target > obj.target )){
+		//prerequisite
+		console.log("change white");
+		document.getElementById(preReq[i][0].course_id).style = "background:white";
+		//source
+		document.getElementById(preReq[i][1]).style = "background:white";
+
+		var ind = preReq.indexOf([preReq[i][0],preReq[i][1]]);
+		preReq.splice(ind, 1);
+		}
+
+    var req = find_in_Used(preReq[i][1]);
+		if( preReq[i][0].course_id == obj.course_id && preReq[i][0].target > req.target ){
+      console.log("change white");
+  		document.getElementById(preReq[i][0].course_id).style = "background:white";
+  		//source
+  		document.getElementById(preReq[i][1]).style = "background:white";
+
+  		var ind = preReq.indexOf([preReq[i][0],preReq[i][1]]);
+  		preReq.splice(ind, 1);
+    }
+
+	  }
+  }
+
+}//end pre()
+
+function find_in_Used(name){
+  for(i in Used){
+    if(Used[i].course_id == name){
+      return Used[i];
+    }
+  }
+}
+function cor(ev){
+  document.getElementById(ev).style = "background:yellow";
+}
+function sem(ev){
+  document.getElementById(ev).style = "background:pink";
+}
+
+
+
+function getCreditsSemester(divID) {
+  var semesterCourses = getChildren(divID);
+  var totalCredits = 0;
+  for (i in semesterCourses) {
+    totalCredits += parseInt(getValue('Credits', semesterCourses[i]));
+  }
+  return totalCredits;
 }
 
 function getValue(colVal, id) {
@@ -80,10 +346,7 @@ function checkSemesters(courseID) {
   }
 }
 
-function checkPrerequisitesMet(courseID) {
-  //Only works if course div is in its semester to be checked
-  //Returns false if the pre/co reqs are not med and true if they are
-  //several lazy shortcuts so if the database is updated we'll need to go through this again
+function checkCorequisitesMet(courseID) {
   for (i in notInDatabase) {
     if (courseID == notInDatabase[i]) {
       return true;
@@ -91,17 +354,15 @@ function checkPrerequisitesMet(courseID) {
   }
 
   var corequisites = getValue('Corequisites', courseID);
-  var prerequisites = getValue('Prerequisites', courseID);
   var curSemester = document.getElementById(courseID).parentNode.id;
-
-  if (corequisites == '-' && prerequisites == '-') {
+  if (corequisites == '-') {
     return true;
   }
-
-  var coreqArray = corequisites.split('+');
-  if (coreqArray == []) {
-    coreqArray = [corequisites];
+  var coreqArray = [corequisites];
+  if (corequisites.indexOf('+') > -1) {
+    coreqArray = corequisites.split('+');
   }
+
   var coreqMetFlag = false;
   var semesterCourses = [];
   for (i in coreqArray) {
@@ -114,13 +375,35 @@ function checkPrerequisitesMet(courseID) {
         for (l in coreqArray) {
           if (coreqArray[l] == semesterCourses[k]) {
             coreqMetFlag = true;
+            return true;
           }
         }
       }
     }
   }
-  if (corequisites == '-') {
-    coreqMetFlag = true;
+  if (coreqMetFlag == false) {
+    return coreqArray;
+  }
+  else {
+    return true;
+  }
+}
+
+function checkPrerequisitesMet(courseID) {
+  //Only works if course div is in its semester to be checked
+  //Returns a 2d array of prerequisites/corequisites if not met
+  //several lazy shortcuts so if the database is updated we'll need to go through this again
+  for (i in notInDatabase) {
+    if (courseID == notInDatabase[i]) {
+      return true;
+    }
+  }
+
+  var prerequisites = getValue('Prerequisites', courseID);
+  var curSemester = document.getElementById(courseID).parentNode.id;
+
+  if (prerequisites == '-') {
+    return true;
   }
 
   var prereqType = '';
@@ -148,7 +431,7 @@ function checkPrerequisitesMet(courseID) {
       semesterCourses = getChildren(semestersArray[j]);
       for (k in semesterCourses) {
         if (prereqArray[i] == semesterCourses[k]) {
-          metArray.push(true);
+          metArray.push(semesterCourses[k]);
           orFlag = true;
         }
       }
@@ -168,11 +451,24 @@ function checkPrerequisitesMet(courseID) {
   if (prerequisites == '-') {
     prereqMetFlag = true;
   }
-  if (coreqMetFlag && prereqMetFlag) {
+  if (prereqMetFlag) {
     return true;
   }
   else {
-    return false;
+    var notMetArray = [];
+    var metFlag = false;
+    for (i in prereqArray) {
+      metFlag = false;
+      for (j in metArray) {
+        if (prereqArray[i] == metArray[j]) {
+          metFlag = true;
+        }
+      }
+      if (metFlag == false) {
+        notMetArray.push(prereqArray[i]);
+      }
+    }
+    return notMetArray;
   }
 }
 
@@ -182,6 +478,9 @@ function h(){
 
   if(start > end || start < 2014){
     alert("Incorrect inputs in Semester Generator section");
+  }
+  else {
+    var newStr = '';
   }
  //else{
    //var x = (end - start)*2;
